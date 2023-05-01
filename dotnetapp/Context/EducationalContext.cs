b561e8c7ae61c5a41c215fe1a5c963c16aa8fcc7
tony20001 <tonymathew2112@gmail.com>
2023-05-01 07:39:29 +0000

    Tony done

---
+++ dotnetapp/Context/EducationalContext.cs
@@ -0,0 +1,26 @@
+﻿using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace dotnetapp.Context
+{
+    public class EducationLoanContext : DbContext
+    {
+        public EducationLoanContext(DbContextOptions<EducationLoanContext> options) : base(options) { }
+
+        public DbSet<UserModel> userModels { get; set; }
+        public DbSet<AdminModel> adminModels { get; set; }
+
+        public DbSet<LoanApplicantModel> loansApplicantModels { get; set; }
+        public DbSet<DocumentModel> documentModels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<UserModel>().HasNoKey();
+
+            modelBuilder.Entity<AdminModel>().HasNoKey();
+        }
+
+    }
+}
---
+++ dotnetapp/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnetapp.Models
+{
+    public class UserModel
+    {
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is Required")]
+        public string Password { get; set; }
+
+        public string UserName { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage ="Role is required")]
+        public string UserRole { get; set; }
+
+    }
+}
